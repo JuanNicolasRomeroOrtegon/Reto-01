28602724fd2b673de11db4f0c7e716aab8f299b5
Juan Nicolás Romero Ortegón <juaromeroor@unal.edu.co>
2025-04-20 20:26:09 -0500

    4_greater_sum_of_consecutives.py

--- 4_greater_sum_of_consecutives.py
+++ 4_greater_sum_of_consecutives.py
@@ -3,7 +3,7 @@ def find_max_consecutive_sum(num_list: list) -> int:
     if len(num_list) < 2:
         raise ValueError("The list must contain at least two elements.")
     
-    greater = num_list[0]
+    greater = num_list[0] + num_list[1]
     for x in range(len(num_list) - 1):
         if num_list[x] + num_list[x+1] > greater:
             greater = num_list[x] + num_list[x+1]
